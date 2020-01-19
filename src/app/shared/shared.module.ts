8aeaa0775a756407145b21349890ff7edcbbb89d
ozaki <fs65ugumori@yahoo.co.jp>
2020-01-19 22:57:14 +0900

    update shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CustomButtonComponent } from '../custom-button/custom-button.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CustomButtonComponent],
+  imports: [CommonModule],
+  exports: [CustomButtonComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
